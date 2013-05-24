21a495f1a2bcf07c0fe52d35efe9d5e4f6204d5e
Emil Wall <emil.wall@valtech.se>
2013-05-24 15:03:05 +0200

    Refactor tests to exploit natural nesting

--- spec/game-spec.js
+++ spec/game-spec.js
@@ -10,14 +10,10 @@ describe("Game", function() {
   it("should have play method", function() {
     expect(typeof this.game.play).toBe("function");
   });
-});
 
-describe("Game.play", function() {
-  beforeEach(function() {
-    this.game = new quiz.Game();
-  });
-
-  it("should require html element as argument", function() {
-    expect(this.game.play).toThrow(new TypeError("element should be an object"));
+  describe("play", function() {
+    it("should require html element as argument", function() {
+      expect(this.game.play).toThrow(new TypeError("element should be an object"));
+    });
   });
 });
